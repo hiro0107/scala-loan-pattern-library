7209071bbb221e258a9fefd11c9b354f723449cd
Hiro <hiro0107@gmail.com>
2011-11-16 13:20:58 +0900

    最初の実装

---
+++ build.sbt
@@ -0,0 +1,7 @@
+organization := "com.github.loanptn"
+
+name := "scala-loan-pattern-library"
+
+version := "0.1.0-SNAPSHOT"
+
+scalaVersion := "2.9.1"
---
+++ src/main/scala/com/github/loanptn/Loan.scala
@@ -0,0 +1,6 @@
+package com.github.loanptn
+
+object Loan {
+  def using[A, R <: { def close }](res: R)(f: (R) => A): A =
+    try { f(res) } finally { res.close }
+}
