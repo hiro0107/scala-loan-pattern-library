9be6ca9ff1cb847bb7653e14362da2194688ea31
Hiro <hiro0107@gmail.com>
2011-11-16 14:02:20 +0900

    package objectに変更

--- src/main/scala/com/github/loanptn/package.scala
+++ src/main/scala/com/github/loanptn/package.scala
@@ -1,6 +1,6 @@
-package com.github.loanptn
+package com.github
 
-object Loan {
+package object loanptn {
   def using[A, R <: { def close() }](res: R)(f: (R) => A): A =
     try { f(res) } finally { res.close }
 }
--- src/test/scala/com/github/loanptn/LoanSuite.scala
+++ src/test/scala/com/github/loanptn/LoanSuite.scala
@@ -1,6 +1,5 @@
 package com.github.loanptn
 
-import com.github.loanptn.Loan._
 import org.mockito.Mockito._
 import org.scalatest.FunSuite
 import org.scalatest.matchers.ShouldMatchers
