4acabd12f018674d6c41c5189cd2451dc909374b
Hiro <hiro0107@gmail.com>
2011-11-17 15:00:23 +0900

    各種リソースクリーナー追加

--- src/main/scala/com/github/loanptn/ResourceCleaner.scala
+++ src/main/scala/com/github/loanptn/ResourceCleaner.scala
@@ -4,13 +4,22 @@ trait ResourceCleaner[R] {
   def clean(res: R): Unit
 }
 
+class CloseableResourceCleaner[R <: { def close() }] extends ResourceCleaner[R]{
+  override def clean(res: R): Unit = res.close()
+}
+
 object ResourceCleaner {
   import java.io._
-  implicit val inputStreamResourceCleaner: ResourceCleaner[InputStream] = new ResourceCleaner[InputStream] {
-    def clean(in: InputStream): Unit = in.close()
-  }
-  implicit val outputStreamResourceCleaner: ResourceCleaner[OutputStream] = new ResourceCleaner[OutputStream] {
-    def clean(in: OutputStream): Unit = in.close()
-  }
+  implicit val inputStreamResourceCleaner = new CloseableResourceCleaner[InputStream]
+  implicit val outputStreamResourceCleaner = new CloseableResourceCleaner[OutputStream]
+
+  import scala.io._
+  implicit val sourceResourceCleaner = new CloseableResourceCleaner[Source]
+
+  import java.sql._
+  implicit val connectionResourceCleaner = new CloseableResourceCleaner[Connection]
+  implicit val statementResourceCleaner = new CloseableResourceCleaner[Statement]
+  implicit val preparedStatementResourceCleaner = new CloseableResourceCleaner[PreparedStatement]
+  implicit val resultSetResourceCleaner = new CloseableResourceCleaner[ResultSet]
 }
 
