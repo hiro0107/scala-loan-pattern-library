663887f0e3d41df684bee6719cbdc6aaf6c8dfb9
Hiro <hiro0107@gmail.com>
2011-11-21 16:52:55 +0900

    flatMapの除去

--- src/main/scala/com/github/loanptn/ManagedResource.scala
+++ src/main/scala/com/github/loanptn/ManagedResource.scala
@@ -1,6 +1,5 @@
 package com.github.loanptn
 
 class ManagedResource[R](res: R, resourceCleaner: ResourceCleaner[R]) {
-  def flatMap[A](f: (R) => ManagedResource[A]): ManagedResource[A] = try { f(res) } finally { resourceCleaner.clean(res) }
   def foreach(f: (R) => Unit): Unit = try { f(res) } finally { resourceCleaner.clean(res) }
 }
