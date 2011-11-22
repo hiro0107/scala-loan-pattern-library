f03aca756c4a6e7159aba38e2b5c31e4209bfa3c
Hiro <hiro0107@gmail.com>
2011-11-22 10:10:28 +0900

    VoidManagedResourceの削除

--- src/main/scala/com/github/loanptn/ManagedResource.scala
+++ src/main/scala/com/github/loanptn/ManagedResource.scala
@@ -1,11 +1,8 @@
 package com.github.loanptn
 
 class ManagedResource[R](res: R, resourceCleaner: ResourceCleaner[R]){
-  class VoidResourceCleaner[S] extends ResourceCleaner[S]{
-    def clean(res: S): Unit = {}
-  }
   def flatMap[B](f: (R) => ManagedResource[B]): ManagedResource[B] = try { f(res) } finally { resourceCleaner.clean(res) }
-  def map[B](f: (R) => B)(implicit cleaner: ResourceCleaner[B] = new VoidResourceCleaner[B]):ManagedResource[B] =
+  def map[B](f: (R) => B)(implicit cleaner: ResourceCleaner[B] = new ResourceCleaner[B] { def clean(res: B): Unit = {} } ):ManagedResource[B] =
     try {
       new ManagedResource(f(res), cleaner)
     } finally {
