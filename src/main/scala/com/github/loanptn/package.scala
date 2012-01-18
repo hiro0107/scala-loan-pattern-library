50e6ae2dfddf01e82f560dd4ad16877cdb01d942
Hiro <hiro0107@gmail.com>
2012-01-18 11:10:30 +0900

    ドキュメントを追加

--- src/main/scala/com/github/loanptn/package.scala
+++ src/main/scala/com/github/loanptn/package.scala
@@ -35,6 +35,25 @@ val x = for(in <- manage(res);
   // do something and return a result
   result
 }
+// このapplyを呼んだ時点で実行がされる
+val result = x()
+ * }}}
+ * 以下のように合成もできる
+ * {{{
+import java.io._
+
+val res: InputStream = ...;
+val res2: InputStream = ...;
+val x1 = for(in <- manage(res)) yield {
+  // do something and return a result
+  result1
+}
+val x2 = for(result1 <- x1;
+            in <- manage(res)) yield {
+  // do something and return a result
+  (result1, result2)
+}
+val (result1, result2) = x2()
  * }}}
  */
 package object loanptn {
